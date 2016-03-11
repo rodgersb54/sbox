6b3e09d025b44690230ea79e75403232e3cc9a82
Ben Rodgers <rodgersb54@gmail.com>
2016-03-10 19:26:22 -0500

    cleanup

--- public/js/app.js
+++ public/js/app.js
@@ -3,42 +3,16 @@ require.config({
 		jquery: '/js/vendor/jquery',
 		moment: '/js/vendor/moment',
 		velocity: '/js/vendor/velocity',
-		Stopwatch: '/js/vendor/stopwatch',
 		utils : '/js/utils/'
 	},
 	shim : {
 		"velocity" : {
 			deps : ["jquery"]
-		},
-		"Stopwatch" : {
-			deps : ["velocity"]
 		}
 	}
 
 });
 
-require([ 'jquery', 'moment', 'velocity', 'Stopwatch' ], function($, moment,  velocity, Stopwatch){
-
-	$('.showtime').on('click', function(){
-		$('.notifications--main').velocity({
-			'right' : '-30'
- 		}, 625, [500, 30]);
-	});
-
-	$('.nav--icon-close').on('click', function(){
-
-		$('.notifications--main').velocity({
-			'right' : '-438'
- 		}, 300, [500, 30]);
-
-	});
-
-
-
-	var $timeDisplay = $('.stopwatch--time-display');
-	//var timer = new Stopwatch( $timeDisplay, { endAt : '2' } );
-	var timer = new Stopwatch( $timeDisplay );
-	//get #'s to work for minutes'
-
+require([ 'jquery', 'moment', 'velocity' ], function($, moment,  velocity ){
 
 });
--- public/js/utils/alertValidation/index.js
+++
@@ -1,9 +0,0 @@
-define('greatModule', function(require, $){
-	console.log('testing2');
-  // Above we have passed in jQuery
-  // They will not be accessible in the global scope
-  return {
-  	'hello' : 'hello back'
-  }
-  // What we return here will be used by other modules
-});
